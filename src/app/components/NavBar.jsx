d6cf6ca624cf84a409d09bdd39e06a1333367ac1
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-18 02:19:50 +0600

    know how to capture multiple sigment

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,24 +1,34 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const NavBar = () => {
-  return (
-    <div>
-      <nav>
-        <ul className="flex justify-around items-center text-lg p-2">
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/services">
-            <li>Services</li>
-          </Link>
-          <Link href="/about">
-            <li>About</li>
-          </Link>
-        </ul>
-      </nav>
-    </div>
-  );
+  const pathname = usePathname();
+  if (!pathname.includes("Dashboard")) {
+    return (
+      <div>
+        <nav>
+          <ul className="flex justify-around items-center text-lg p-2">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/services">
+              <li>Services</li>
+            </Link>
+            <Link href="/about">
+              <li>About</li>
+            </Link>
+            <Link href="/contact">
+              <li>Contact Us</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default NavBar;
---
+++ src/app/contact/[...slugs]/page.jsx
@@ -0,0 +1,5 @@
+export default async function ContactSlugsPage({ params }) {
+  const p = await params;
+  console.log(p);
+  return <div></div>;
+}
